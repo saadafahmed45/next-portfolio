eacfa03528948ec1cb829972399508aa1866b863
saadafahmed45 <hafijnahid45@gmail.com>
2024-11-23 03:03:46 +0600

    added wp massanger

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
+"use client";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 import AboutPage from "./about/page";
 import CardHover from "./components/CardHover";
 import HeroPage from "./components/Hero";
 import ContactPage from "./contact/page";
 import PortfolioPage from "./portfolio/page";
-
 export default function Home() {
   return (
     <main>
+      <FloatingWhatsApp
+        phoneNumber="+8801890195058"
+        accountName="Mohammad Haolader"
+        // avatar="/path/to/avatar.png" // Optional: You can add your custom avatar image
+        chatMessage="Hello there! How can we assist you today?" // Default message
+        statusMessage="Typically replies within 1 hour"
+        placeholder="Type your message here..."
+        // allowClickAway // Close chat when clicking outside
+        notification // Show a notification bubble
+        notificationSound // Play a sound for notifications
+      />
       <HeroPage />
       {/* <CardHover /> */}
       <AboutPage />
